95932c405735bdc08329fdd886ded32cb438f35a
JirayutW <11388287@krungsri.com>
2022-06-25 13:45:24 +0700

    initial unitest course

---
+++ MLOAN.TDD.Exercise1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MLOAN.TDD.Exercise1
+{
+    public class NumberHelper
+    {
+        public int[] FindMissing(int[] values)
+        {
+            //return new int[] { 1,2,3 };
+
+            if (values == null) throw new ArgumentNullException(nameof(values));
+            if (values.Length == 0) return new int[0];
+            Array.Sort(values);
+            for (int last = values[0], i = 1; last < values.Length; last++)
+            {
+                if (values[i] != last + 1) return new[] { last + 1 };
+                last = values[i];
+            }
+            return new int[] { };
+        }
+    }
+    //public List<int> TestMethod(){
+    //    var res = new List<int>();
+
+
+    //    return res;
+    //}
+}
---
+++ MLOAN.TDD.Exercise1/MLOAN.TDD.Exercise1Test/UnitTest1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Xunit;
+using MLOAN.TDD.Exercise1;
+namespace MLOAN.TDD.Exercise1Test
+{
+    public class NumberHelperTest
+    {
+        public class FindMissings
+        {
+            [Fact]
+            public void Continuously_NoMissing()
+            {
+               //arrange
+               var values = new[] {1,2,3 };
+                var sut = new NumberHelper();
+                //act
+                int[] res = sut.FindMissing(values);
+
+                //assert
+                Assert.NotNull(res);
+                Assert.Equal(0, res.Length);
+
+            }
+
+            [Fact]
+            public void ContinuouslyNoSorted_NoMissing()
+            {
+                //arrange
+                var values = new[] { 1, 3, 2 };
+                var sut = new NumberHelper();
+                //act
+                int[] res = sut.FindMissing(values);
+
+                //assert
+                Assert.NotNull(res);
+                Assert.Equal(0, res.Length);
+            }
+
+            [Fact]
+            public void HasSingleMissing()
+            {
+                //arrange
+                var values = new[] { 1, 3, 2,5 };
+                var sut = new NumberHelper();
+                //act
+                int[] res = sut.FindMissing(values);
+
+                //assert
+                Assert.NotNull(res);
+                Assert.Single(res);
+
+                Assert.Equal(new[] { 4 },res );
+            }
+
+            [Fact]
+            public void EmptyArray_NoMissing()
+            {
+                //arrange
+                var values = new int[0];
+                var sut = new NumberHelper();
+                //act
+                int[] res = sut.FindMissing(values);
+
+                //assert
+                Assert.NotNull(res);
+                Assert.Empty(res);
+            }
+            [Fact]
+            public void Null_ThrowException()
+            {
+                //arrange
+                int[]? values = null;
+                var sut = new NumberHelper();
+                //act
+                var ex = Assert.Throws<ArgumentNullException>(() => {
+                    sut.FindMissing(values);
+                });
+                //assert
+
+                Assert.Equal("values", ex.ParamName);
+            }
+
+        }
+
+    }
+}
